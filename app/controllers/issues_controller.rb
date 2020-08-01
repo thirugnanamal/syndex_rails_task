bbdf0e0e7f6794bbbda21e68c34000a5935fa18d
thirugnanamal <thirugnanamal.1994@gmail.com>
2020-08-01 15:14:16 +1200

    logic for saving an application

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -8,4 +8,17 @@ class IssuesController < ApplicationController
   def index
     @issues = Issue.all
   end
+  def apply
+  	@issue = Issue.find(params[:issue_id])
+  end
+
+  def application
+    params[:current_user].present?  ? @account = Account.find(params[:current_user]) : @account = Account.find(params[:account_id])
+    if @account.verified?
+      @application = Application.create(account_id: @account.id, issue_id: params[:issue_id], requester_id: current_account.id)
+    end
+    respond_to do |format|
+      format.js
+    end
+  end
 end
